73c8018c8dc687844f14ed080cacaa49a7648a50
Sverre Nystad <sverrenystad@gmail.com>
2023-08-09 14:41:27 +0200

    Refactor: Make more readeble

--- merge_strings_alternately.py
+++ merge_strings_alternately.py
@@ -3,16 +3,18 @@ class Solution:
         """
         You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. 
         If a string is longer than the other, append the additional letters onto the end of the merged string.
+
         Return the merged string.
         """
-        merged = []
-        for w1, w2 in zip(word1, word2):
-            merged.extend([w1, w2])
+        merged = ""
 
-        # Add remaining
-        _min: int = min(len(word1), len(word2))
-        if len(word1) <= len(word2):
-            merged.extend(word2[_min:])
-        else:
-            merged.extend(word1[_min:])
-        return ''.join(merged)
+        limit: int = min(len(word1), len(word2))
+        longerWord = word2 if len(word1) <= len(word2) else word1
+
+        for i in range(limit):
+            merged += word1[i] + word2[i]
+
+        # Add remaining part
+        merged += longerWord[limit:]
+
+        return merged
