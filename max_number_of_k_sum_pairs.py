ea8ecd6e57d1c8159b27d705b8a341ef7cc228b4
SverreNystad <sverrenystad@gmail.com>
2024-12-08 23:51:29 +0100

    refactor: change i and j into left and right

--- max_number_of_k_sum_pairs.py
+++ max_number_of_k_sum_pairs.py
@@ -5,24 +5,18 @@ class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         operations = 0
 
-        # Create a dictionary to store the frequency of each number
+        left = 0
+        right = len(nums) - 1
+        nums.sort()
+        while left < right:
+            if nums[left] + nums[right] == k:
+                left += 1
+                right -= 1
+                operations += 1
 
-        freq = {}
-        for num in nums:
-            if num in freq:
-                freq[num] += 1
+            elif nums[left] + nums[right] > k:
+                right -= 1
+
             else:
-                freq[num] = 1
-
-        # Iterate through the dictionary and check if the complement of the current number exists in the dictionary
-        # If it does, increment the operations count and decrement the frequency of the current number and its complement
-        for num in freq:
-            complement = k - num
-            if complement in freq:
-                if complement == num:
-                    operations += freq[num] // 2
-                else:
-                    operations += min(freq[num], freq[complement])
-                    freq[num] = 0
-                    freq[complement] = 0
+                left += 1
         return operations
