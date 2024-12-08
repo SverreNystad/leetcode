7a2ec6e9723f6c352b72e6408a2bc7909ff2fdb2
SverreNystad <sverrenystad@gmail.com>
2024-12-09 00:13:16 +0100

    refactor: optimize findMaxAverage method for efficiency

--- maximum_average_subarray_i.py
+++ maximum_average_subarray_i.py
@@ -1,18 +1,9 @@
 from typing import List
 
-from numpy import mean
-
 
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-
-        all_subarrays = []
-        for i in range(len(nums) - k + 1):
-            all_subarrays.append(nums[i : i + k])
-
         max_average = float("-inf")
-        for subarray in all_subarrays:
-            average = mean(subarray)
-            if average > max_average:
-                max_average = average
+        for i in range(len(nums) - k + 1):
+            max_average = max(max_average, sum(nums[i : i + k]) / k)
         return max_average
