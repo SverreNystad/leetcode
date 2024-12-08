a01762bc1d2a03f5a52c0aad6a6113dc9c6ba252
SverreNystad <sverrenystad@gmail.com>
2024-12-09 00:19:52 +0100

    refactor: do division at end for findMaxAverage method for efficiency

--- maximum_average_subarray_i.py
+++ maximum_average_subarray_i.py
@@ -5,5 +5,5 @@ class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         max_average = float("-inf")
         for i in range(len(nums) - k + 1):
-            max_average = max(max_average, sum(nums[i : i + k]) / k)
-        return max_average
+            max_average = max(max_average, sum(nums[i : i + k]))
+        return max_average / k
