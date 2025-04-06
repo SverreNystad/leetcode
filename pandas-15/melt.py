91cddbb37c13246bf7d7952ea9adcd99a77879d2
Sverre Nystad <sverrenystad@gmail.com>
2025-04-06 22:55:15 +0200

    feat: create pandas melt of unpivot task

---
+++ pandas-15/melt.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    return report.melt(
+        id_vars="product",
+        var_name="quarter",
+        value_name="sales",
+    )
