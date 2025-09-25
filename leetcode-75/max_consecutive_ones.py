836ec9999faf5b66f6db68ef439fab7276107cbb
Sverre Nystad <sverrenystad@gmail.com>
2025-09-26 00:16:34 +0200

    feat: implement longestOnes function to calculate maximum consecutive 1's with k flips

--- leetcode-75/max_consecutive_ones.py
+++ leetcode-75/max_consecutive_ones.py
@@ -1,3 +1,23 @@
+"""
+
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+"""
+
+
 class Solution:
     def longestOnes(self, nums: list[int], k: int) -> int:
-        return 6
+        left = 0
+        max_length = 0
+        zero_count = 0
+
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zero_count += 1
+
+            while zero_count > k:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left += 1
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
