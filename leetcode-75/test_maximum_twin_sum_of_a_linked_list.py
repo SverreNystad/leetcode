ebea5ea5410c31b0fefa49c8575077b0c73a4328
SverreNystad <sverrenystad@gmail.com>
2026-07-21 12:44:36 +0200

    test: add additional test case for maximum twin sum of a linked list

--- leetcode-75/test_maximum_twin_sum_of_a_linked_list.py
+++ leetcode-75/test_maximum_twin_sum_of_a_linked_list.py
@@ -8,6 +8,7 @@ import pytest
         (ListNode(5, ListNode(4, ListNode(2, ListNode(1)))), 6),
         (ListNode(4, ListNode(2, ListNode(2, ListNode(3)))), 7),
         (ListNode(1, ListNode(100000)), 100001),
+        (ListNode(1, ListNode(2, ListNode(100000))), 100001),
     ],
 )
 def test_maximum_twin_sum_of_a_linked_list(head, expected):
